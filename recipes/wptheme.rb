4bb8965f557ffd33e67f073b309d10649831160b
Aleh Belavusau <Aleh_Belavusau@epam.com>
2016-06-23 13:29:13 +0300

    test removal

--- recipes/wptheme.rb
+++
@@ -1,8 +0,0 @@
-
-
-template "/var/www/wordpress/wp-includes/default-constants.php" do
-  source "default-constants.php.erb"
-  variables({
-    :default_theme => node['wordpress']['themes']['default']
-  })
-end
